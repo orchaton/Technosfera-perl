8f2e9b0ff7d8dc00d4cee78a2255b7bd2bcd7408
Vladislav Grubov <vladgrubov@gmail.com>
2016-03-08 22:58:35 +0300

    GetterSetter Release v2.0 Solved without 'eval'

--- homeworks/getset/lib/Local/GetterSetter.pm
+++ homeworks/getset/lib/Local/GetterSetter.pm
@@ -3,6 +3,8 @@ package Local::GetterSetter;
 use strict;
 use warnings;
 
+use feature 'say';
+
 =encoding utf8
 
 =head1 NAME
@@ -18,36 +20,14 @@ Version 1.00
 our $VERSION = '1.00';
 
 sub import {
-  my($me, @fields) = @_;
-  return unless @fields;
+  my ($self, @fields) = @_;
+  my $caller =  caller();
 
-  my $caller = caller();  
-  my $eval = "package $caller;\n";
-
-  foreach my $field (@fields) {
-    $eval .= "sub get_$field { return \${$caller::$field}; }\n";
-    $eval .= "sub set_$field { \${$caller::$field} = shift; }\n";
+  no strict 'refs';
+  for my $field (@fields) {
+    *{"$caller"."::get_$field"} = sub { return ${"$caller::$field"}; };
+    *{"$caller"."::set_$field"} = sub { ${"$caller::$field"} = shift; };
   }
-
-  eval $eval;
-
-  # $@ содержит возможные ошибки вычисления
-  $@ and die "Error for $caller: $@";
 }
-
-=head1 SYNOPSIS
-
-    package Local::SomePackage;
-    use Local::GetterSetter qw(x y);
-
-    set_x(50);
-    print our $x; # 50
-
-    our $y = 42;
-    print get_y(); # 42
-    set_y(11);
-    print get_y(); # 11
-
-=cut
 
 1;
