78cc2a0b112307777ccf78ecb950f2d5dbb8f0b1
Vladislav Grubov <vladgrubov@gmail.com>
2016-03-14 19:37:14 +0300

    Library Files were modified

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -9,7 +9,9 @@ use FindBin;
 
 require "$FindBin::Bin/../lib/Local/PathParser.pm";
 require "$FindBin::Bin/../lib/Local/FilterSort.pm";
+require "$FindBin::Bin/../lib/Local/TablePrint.pm";
 require "$FindBin::Bin/../lib/Local/PrepareForPrint.pm";
+
 
 # Получение ключей запуска
 my %keys = %{Local::PathParser::getExecOptions()};
@@ -30,44 +32,4 @@ my @ready_lib = @{Local::PrepareForPrint::makeList(\@sorted_lib, $keys{columns})
 my @required_space = @{Local::PrepareForPrint::necessarySpace(\@ready_lib, scalar(@{$keys{columns}}))};
 
 # Вывод библиотеки на экран
-printLib(\@ready_lib, \@required_space);
-
-sub printLib {
-	my ($lib, $space) = @_;
-	my $lib_len = scalar(@$lib);
-	
-	if ($lib_len == 0 || scalar(@$space) == 0) {	# краевые значения
-		return 0;
-	}
-
-	my $head_line = "/" . (join "", (map {"-"x($_ + 3)} @$space));
-	chop ($head_line);
-	$head_line .= "\\";
-
-	my $skip_line = "\n|" . (join "+", (map {"-"x($_ + 2)} @$space)) . "|\n";
-
-	say $head_line;
-
-	for my $line_iter (0..$lib_len-1) {
-		
-		my @arr = @{$lib->[$line_iter]};
-		
-		print "|";
-		for my $iter (0..$#arr) {
-			print map {" "} ( 0..($space->[$iter] - length $arr[$iter]) );
-			print $arr[$iter];
-			print " |";
-		}
-		if ($line_iter < $lib_len - 1){			# проверка на последнюю строку.
- 			print $skip_line;
- 		} else {								# не нужно выводить $skip_line
- 			print "\n";
- 		}
-	}
-
-	my $bottom_line = "\\" . (join "", (map {"-"x($_ + 3)} @$space));
-	chop($bottom_line);
-	$bottom_line .= "/";
-
-	say $bottom_line;
-}
+TablePrint::printLib(\@ready_lib, \@required_space);
