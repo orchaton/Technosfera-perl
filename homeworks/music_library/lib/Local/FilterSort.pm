7549f66a0c59bbb1d090c8a48462b3631fc3b8ef
Vladislav Grubov <vladgrubov@gmail.com>
2016-03-14 23:31:24 +0300

    codestyle update FilterSort.pm

--- homeworks/music_library/lib/Local/FilterSort.pm
+++ homeworks/music_library/lib/Local/FilterSort.pm
@@ -27,43 +27,43 @@ our $VERSION = '1.00';
 # Функция принимает указатель на массив хешей (библиотеку музыки) и указатель на %keys
 # Функция возвращает указатель на массив хешей (отфильтрованная библиотека)
 sub filterLib {
-	my ($lib, $keys) = @_;
+    my ($lib, $keys) = @_;
 
-	my @necessary_keys = grep {defined($keys->{$_}) and $_ ne 'sort' and $_ ne 'columns'} keys %$keys;	
-	my @filtered_lib;
-	
-	sub num_comparator { my ($a, $b) = @_; return $a != $b; }
-	sub str_comparator { my ($a, $b) = @_; return $a ne $b; }
+    my @necessary_keys = grep {defined($keys->{$_}) and $_ ne 'sort' and $_ ne 'columns'} keys %$keys;    
+    my @filtered_lib;
+    
+    sub num_comparator { my ($a, $b) = @_; return $a != $b; }
+    sub str_comparator { my ($a, $b) = @_; return $a ne $b; }
 
-	my $comparators = {	year => \&num_comparator,
-						band => \&str_comparator,
-						album => \&str_comparator,
-						track => \&str_comparator,
-						format => \&str_comparator
-						};
-	
-	for my $node (@$lib) {
-		
-		my $f = 1;                                            # flag garanties that all keys were passed
-		for my $key (@necessary_keys) {
-			$f = 0 if $comparators->{$key} ($node->{$key}, $keys->{$key});
-		}
-		push @filtered_lib, $node if $f;
-	}
+    my $comparators = { year => \&num_comparator,
+                        band => \&str_comparator,
+                        album => \&str_comparator,
+                        track => \&str_omparator,
+                        format => \&str_comparator
+                        };
+    
+    for my $node (@$lib) {
+        
+        my $f = 1;                                            # flag garanties that all keys were passed
+        for my $key (@necessary_keys) {
+            $f = 0 if $comparators->{$key} ($node->{$key}, $keys->{$key});
+        }
+        push @filtered_lib, $node if $f;
+    }
 
     return \@filtered_lib;
 }
 
 # Функция сортирует входную библиотеку согласно $keys{sort}
 sub sortLib {
-	my ($lib, $keys) = @_;
+    my ($lib, $keys) = @_;
 
-	if ($keys->{sort} eq "year") {
-		@$lib = sort { $a->{$keys->{sort}} <=> $b->{$keys->{sort}} } @$lib;	
-	} else {
-		@$lib = sort { $a->{$keys->{sort}} cmp $b->{$keys->{sort}} } @$lib;
-	}
-	return $lib;
+    if ($keys->{sort} eq "year") {
+        @$lib = sort { $a->{$keys->{sort}} <=> $b->{$keys->{sort}} } @$lib;    
+    } else {
+        @$lib = sort { $a->{$keys->{sort}} cmp $b->{$keys->{sort}} } @$lib;
+    }
+    return $lib;
 }
 
 1;
