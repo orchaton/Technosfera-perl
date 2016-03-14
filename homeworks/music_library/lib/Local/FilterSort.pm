2247df848f45acf7be982719a96c81780125c73a
Vladislav Grubov <vladgrubov@gmail.com>
2016-03-15 00:46:17 +0300

    Codestyle

--- homeworks/music_library/lib/Local/FilterSort.pm
+++ homeworks/music_library/lib/Local/FilterSort.pm
@@ -30,7 +30,7 @@ sub num_sorter { my ($a, $b, $k) = @_; return $a->{$k} <=> $b->{$k}; }
 sub str_sorter { my ($a, $b, $k) = @_; return $a->{$k} cmp $b->{$k}; }
 
 my $comparators = { year   => {compare => \&num_comparator, sort => \&num_sorter},
-					band   => {compare => \&str_comparator, sort => \&str_sorter},
+                    band   => {compare => \&str_comparator, sort => \&str_sorter},
                     album  => {compare => \&str_comparator, sort => \&str_sorter},
                     track  => {compare => \&str_omparator,  sort => \&str_sorter},
                     format => {compare => \&str_comparator, sort => \&str_sorter}
