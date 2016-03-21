00a44582047fc3692a3a1dd0838807fae481b82b
Vladislav Grubov <orchaton@github.com>
2016-03-21 11:57:42 +0300

    Initial Commit

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -9,6 +9,16 @@ our @EXPORT = qw( parse_json );
 sub parse_json {
 	my $source = shift;
 	
+	my $num = qr/
+		\-?
+		(?:[1-9]\d*|0)
+		(?:\.\d+)?
+		(?:[eE][\+\-]?\d+)?
+	/x;
+
+	
+
+
 	use JSON::XS;
 	
 	# return JSON::XS->new->utf8->decode($source);
