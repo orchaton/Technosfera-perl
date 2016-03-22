413c16686b1324ab15fe35b187e12769b4be201f
Vladislav Grubov <orchaton@github.com>
2016-03-22 16:34:09 +0300

    Codestyle

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -11,132 +11,135 @@ use Encode;
 use utf8;
 
 BEGIN{
-	if ($] < 5.018) {
-		package experimental;
-		use warnings::register;
-	}
+    if ($] < 5.018) {
+        package experimental;
+        use warnings::register;
+    }
 }
 no warnings 'experimental';
 use feature 'say';
 use DDP;
 
 sub parse_str {
-	$_ = shift;
-	chop;				# delete last "
-	s/"//;				# delete first "
+    $_ = shift;
+    chop;                # delete last "
+    s/"//;               # delete first "
 
-	s/\\n/\n/g;
-	s/\\t/\t/g;
-	s/\\b/\b/g;
-	s/\\f/\f/g;
-	s/\\r/\r/g;
+    s/\\n/\n/g;
+    s/\\t/\t/g;
+    s/\\b/\b/g;
+    s/\\f/\f/g;
+    s/\\r/\r/g;
 
-	s/\\u([0-9a-fA-F]{4})/chr(hex($1))/ge;
-	return $_;
+    s/\\u([0-9a-fA-F]{4})/chr(hex($1))/ge;
+    return $_;
 }
 
 sub parse_num {
-	$_ = shift;
-	return (0+$_);
+    $_ = shift;
+    return (0+$_);
 }
 
 sub parse_var {
-	$_ = shift;
-	if (/^false$/sg) {
-		return "";
-	}
-	if (/^true$/sg) {
-		return 1;
-	}
-	if (/^null$/sg) {
-		return undef;
-	}
-	0;
+    $_ = shift;
+    if (/^false$/sg) {
+        return "";
+    }
+    if (/^true$/sg) {
+        return 1;
+    }
+    if (/^null$/sg) {
+        return undef;
+    }
+    0;
 }
 
 sub parse_json {
-	my $source = shift;	
-	my $num = qr/
-		\-?										# unary
-		(?:[1-9]\d*|0)							# integer part
-		(?:\.\d+)?								# fixed float part
-		(?:[eE][\+-]?\d+)?						# eE float part
-	/x;
+    my $source = shift;    
+    my $num = qr/
+        \-?                                       # unary
+        (?:[1-9]\d*|0)                            # integer part
+        (?:\.\d+)?                                # fixed float part
+        (?:[eE][\+-]?\d+)?                        # eE float part
+    /x;
 
-	my $str = qr/
-		\"										# begin of string
-		(?:
-		[^"\\]									# unicode char except \" and \\
-		|
-		\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})		# controlling symbols
-		)*
-		\"										# end of string
-	/x;
-	my $var = qr/(?:true|false|null)/;
+    my $str = qr/
+        \"                                        # begin of string
+        (?:
+        [^"\\]                                    # unicode char except \" and \\
+        |
+        \\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})        # controlling symbols
+        )*
+        \"                                        # end of string
+    /x;
+    my $var = qr/(?:true|false|null)/;
 
-	my %smart_patterns = (
-		number => \{pattern => $num, parser => \&parse_num},
-		string => \{pattern => $str, parser => \&parse_str},
-		var    => \{pattern => $var, parser => \&parse_var}
-		);
+    my %smart_patterns = (
+        number => \{pattern => $num, parser => \&parse_num},
+        string => \{pattern => $str, parser => \&parse_str},
+        var    => \{pattern => $var, parser => \&parse_var}
+        );
 
-	# Easy parsers:
-	for (values %smart_patterns) {
-		if ($source =~ /^(${$$_}{pattern})(.*)/sg) {
-			return wantarray ? (${$$_}{parser}($1), $2) : ${$$_}{parser}($1);
-		}
-	}
+    # Easy parsers:
+    for (values %smart_patterns) {
+        if ($source =~ /^(${$$_}{pattern})(.*)/sg) {
+            return wantarray ? (${$$_}{parser}($1), $2) : ${$$_}{parser}($1);
+        }
+    }
 
-	if ($source =~ /\G\s*\{\s*(.*)/sgc) {
-		my %h;
-		$source = $1;
-		while ($source =~ /\w/sg){
+    # Hash parser:
+    if ($source =~ /\G\s*\{\s*(.*)/sgc) {
+        my %h;
+        $source = $1;
+        while ($source =~ /\w/sg){
 
-			my $key;
-			my $val;
-			($key, $source) = parse_json($source);
+            my $key;
+            my $val;
+            ($key, $source) = parse_json($source);
 
-			$source =~ s/\s*:\s*//;
+            $source =~ s/\s*:\s*//;
 
-			($val, $source) = parse_json($source);
-			$h{$key} = $val;
+            ($val, $source) = parse_json($source);
+            
+            $h{$key} = $val;
 
-			$source =~ s/^\s*,\s*//;
-			if ($source =~ s/^\s*\}//) {
-				last;
-			}
-		}
-		return wantarray ? (\%h, $source) : \%h;
-	}
+            $source =~ s/^\s*,\s*//;
+            if ($source =~ s/^\s*\}//) {
+                last;
+            }
+        }
+        return wantarray ? (\%h, $source) : \%h;
+    }
 
-	if ($source =~ /\G\s*\[\s*(.*)/sgc) {
-		my @arr;
+    # Array parser:
+    if ($source =~ /\G\s*\[\s*(.*)/sgc) {
+        my @arr;
 
-		$source = $1;
-		while ($source =~ /\w/sg){
+        $source = $1;
+        while ($source =~ /\w/sg){
 
-			my $val;
-			($val, $source) = parse_json($source);
+            my $val;
+            ($val, $source) = parse_json($source);
 
-			push @arr, $val;
+            push @arr, $val;
 
-			$source =~ s/^\s*,\s*//;
-			if ($source =~ s/^\s*\]//) {
-				last;
-			}
-		}
-		return wantarray ? (\@arr, $source) : \@arr
-	}
+            $source =~ s/^\s*,\s*//;
+            if ($source =~ s/^\s*\]//) {
+                last;
+            }
+        }
+        return wantarray ? (\@arr, $source) : \@arr
+    }
 
-	
-	return {};
+    
+    return {};
 }
 
 sub parse_json_xs {
-	my $source = shift;
-	use JSON::XS;
+    my $source = shift;
+    use JSON::XS;
 
-	return JSON::XS->new->utf8->decode($source);
+    return JSON::XS->new->utf8->decode($source);
 }
 
 # p parse_json('"Моя\tстрока!\n\u0048"');
