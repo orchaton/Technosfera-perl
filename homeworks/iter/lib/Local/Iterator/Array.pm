0aeb28d102657958de0deedf9c6d5dcfad8f8a4c
Vladislav Grubov <gru212@tproger.ru>
2016-04-08 00:09:20 +0300

    Iterator. Array Iterator done.

--- homeworks/iter/lib/Local/Iterator/Array.pm
+++ homeworks/iter/lib/Local/Iterator/Array.pm
@@ -1,5 +1,4 @@
 package Local::Iterator::Array;
-
 use strict;
 use warnings;
 
@@ -7,12 +6,51 @@ use warnings;
 
 =head1 NAME
 
-Local::Iterator::Array - array-based iterator
+Local::Iterator::Array - iterator of array
 
 =head1 SYNOPSIS
 
-    my $iterator = Local::Iterator::Array->new(array => [1, 2, 3]);
+    my $iterator = Local::Iterator::Array->new(
+        array => [1, 2, 3]
+    );
 
 =cut
+
+
+use Mouse;
+
+our $VERSION = '1.0';
+
+has 'array' => (
+    'is' => 'rw',
+    'isa' => 'Item'
+);
+
+has 'last' => (
+    'is' => 'rw',
+    'isa' => 'Int'
+);
+
+sub BUILD {
+    my ($self) = @_;
+    $self->last(0);
+    return ;
+}
+
+sub next {
+    my ($self) = @_;
+    if ($self->last == scalar @{$self->array}) {
+        return (undef, 1);
+    }
+    $self->last($self->last + 1);
+    return ($self->array->[$self->last - 1], 0);
+}
+
+sub all {
+    my ($self) = @_;
+    my @res = map { $self->array->[$_]  } $self->last .. scalar @{$self->array} - 1;
+    $self->last ( scalar @{$self->array} );
+    return \@res;
+}
 
 1;
